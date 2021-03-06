53a36fea0dea539a3fc293cf80213d634c9f3b71
zaid837 <ayomide.zaid@gmail.com>
2021-03-06 18:37:26 +0100

    added responsive navigation

---
+++ sctipt.js
@@ -0,0 +1,5 @@
+function menu () {
+    document.getElementById('overlay').classList.toggle('overlay')
+    document.getElementById('mobile-menu').classList.toggle('show')
+    document.getElementById('close').classList.toggle('m-close')
+}
